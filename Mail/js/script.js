8e843dc9675902f6ed86d833e073cf2c9cfdff64
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-06-28 16:42:38 +0200

    [UPD] defined a variable that gets the button logIn

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,2 +1,4 @@
 // DEFINISCO L'ARRAI CON UNA LISTA DI EMAIL INVENTATE
-let emailList = ["mattia@boolean.com", "doubleorquits@luckydice.com", "diceshake@boolean.com", "dicerhythm@boolean.com", "diceroll@boolean.com", "highroller@boolean.com", "dadifunziona@boolean.com", "dadisvelati@boolean.com", "dadi@boolean.com"] 
+let emailList = ["mattia@boolean.com", "doubleorquits@luckydice.com", "diceshake@boolean.com", "dicerhythm@boolean.com", "diceroll@boolean.com", "highroller@boolean.com", "dadifunziona@boolean.com", "dadisvelati@boolean.com", "dadi@boolean.com"];
+// RECUPERO IL PULSANTE DI ACCESSO
+const button = document.getElementById('logIn');
