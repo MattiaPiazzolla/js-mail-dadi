50be6b3a48f60c67d21c42df917fe05b1daa1570
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-06-28 16:46:33 +0200

    [UPD] added event listener for the logInButton

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,4 +1,8 @@
 // DEFINISCO L'ARRAI CON UNA LISTA DI EMAIL INVENTATE
 let emailList = ["mattia@boolean.com", "doubleorquits@luckydice.com", "diceshake@boolean.com", "dicerhythm@boolean.com", "diceroll@boolean.com", "highroller@boolean.com", "dadifunziona@boolean.com", "dadisvelati@boolean.com", "dadi@boolean.com"];
 // RECUPERO IL PULSANTE DI ACCESSO
-const button = document.getElementById('logIn');
+const logInButton = document.getElementById('logIn');
+// DETERMINO LE ISTRUZIONI PER UN EVENTO DEL PULSANTE
+logInButton.addEventListener('click', function(){
+    
+})
