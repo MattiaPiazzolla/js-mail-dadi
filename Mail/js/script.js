2495cd6fc69d39359fffb92e48c6f984e6b4c5ae
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-06-28 16:39:05 +0200

    [UPD] defined the array emailList

---
+++ Mail/js/script.js
@@ -0,0 +1,2 @@
+// DEFINISCO L'ARRAI CON UNA LISTA DI EMAIL INVENTATE
+let emailList = ["mattia@boolean.com", "doubleorquits@luckydice.com", "diceshake@boolean.com", "dicerhythm@boolean.com", "diceroll@boolean.com", "highroller@boolean.com", "dadifunziona@boolean.com", "dadisvelati@boolean.com", "dadi@boolean.com"] 
